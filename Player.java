f2c9cc8e5a4096fe1092b30d5471ff88fe5ac3a4
Woodsale <woodsale@mail.gvsu.edu>
2018-11-09 13:14:13 -0500

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,20 +1,22 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Player extends Rectangle{
 	
 	public boolean up, down, left, right;
 	/*Speed is how many pixels every tick
-	 * whcih should be at 60 fps*/
+	 * whcih should be at 180 fps*/
 	private int speed = 1;
-	
+	private static int a = 0,b = 0,moveX = 0,moveY = 0;
 	private int size;
+	
+	Random ran = new Random();
 	
 	/**/
 	public Player(int x, int y) {
 		size = 20;
 		setBounds(x,y,size,size);//locx,locy,sizex,sizey
-	
 	}
 	
 	/*Allows for movement*/
@@ -24,8 +26,15 @@ public class Player extends Rectangle{
 		ArrayList<Rectangle> list = new ArrayList<Rectangle>();
 		list = Map.getMap();
 		
+		/*b++;
+		if(b > 180) {
+			moveX = ran.nextInt(2);
+			moveY = ran.nextInt(2);
+			b = 0;
+		}*/
+		
 		boolean cx = false, cy = false;
-		if(right) {
+		if(right) {//moveX=0
 			for(int i = 0;i<list.size();i++) {
 				r = list.get(i);
 				if(collision(x+speed,y,r) == true){
@@ -34,12 +43,9 @@ public class Player extends Rectangle{
 			}
 			if(cx == false){
 				x+=speed;
-			}/*
-			if(collision(x+speed,y,r) == false){
-				x+=speed;
-			}*/
+			}
 		}
-		if(left) {
+		if(left) {//left//moveX==1
 			for(int i = 0;i<list.size();i++) {
 				r = list.get(i);
 				if(collision(x-speed,y,r) == true){
@@ -48,12 +54,9 @@ public class Player extends Rectangle{
 			}
 			if(cx == false){
 				x-=speed;
-			}/*
-			if(collision(x-speed,y,r) == false){
-				x-=speed;
-			}*/
+			}
 		}
-		if(up) {
+		if(up) {//up//moveY==0
 			for(int i = 0;i<list.size();i++) {
 				r = list.get(i);
 				if(collision(x,y-speed,r) == true){
@@ -62,12 +65,9 @@ public class Player extends Rectangle{
 			}
 			if(cy == false){
 				y-=speed;
-			}/*
-			if(collision(x,y-speed,r) == false){
-				y-=speed;
-			}*/
+			}
 		}
-		if(down) {
+		if(down) {//down//moveY==0
 			for(int i = 0;i<list.size();i++) {
 				r = list.get(i);
 				if(collision(x,y+speed,r) == true){
@@ -76,10 +76,7 @@ public class Player extends Rectangle{
 			}
 			if(cy == false){
 				y+=speed;
-			}/*
-			if(collision(x,y+speed,r) == false){
-				y+=speed;
-			}*/
+			}
 		}
 
 		//System.out.println(getLocation()+" " + x + " "+ y);
@@ -100,15 +97,52 @@ public class Player extends Rectangle{
 		g.fillRect(x, y, width, height);
 		
 		/*Upper Left corner*/
-		/*g.setColor(Color.BLACK);
-		g.fillRect(x, y, 8,1);
 		g.setColor(Color.BLACK);
-		g.fillRect(x, y, 1,8);
-		g.setColor(Color.BLACK);
-		g.fillRect(x, y, 2,5);
-		g.setColor(Color.BLACK);
+		g.fillRect(x, y, 7,1);
+		g.fillRect(x, y, 1,7);
+		g.fillRect(x, y, 3, 3);
 		g.fillRect(x, y, 5,2);
-		g.setColor(Color.BLACK);
-		g.fillRect(x, y, 3, 3);*/
+		g.fillRect(x, y, 2,5);
+		
+		/*Lower Right corner*/
+		g.fillRect(width+x-7, height+y-1, 7,1);
+		g.fillRect(width+x-1, height+y-7, 1,7);
+		g.fillRect(width+x-3, height+y-3, 3,3);
+		g.fillRect(width+x-5, height+y-2, 5,2);
+		g.fillRect(width+x-2, height+y-5, 2,5);
+		
+		/*Upper Right corner*/
+		g.fillRect(width+x-7, y, 7,1);
+		g.fillRect(width+x-1, y, 1,7);
+		g.fillRect(width+x-3, y, 3,3);
+		g.fillRect(width+x-5, y, 5,2);
+		g.fillRect(width+x-2, y, 2,5);
+		
+		/*Lower Left corner*/
+		g.fillRect(x, height+y-1, 7,1);
+		g.fillRect(x, height+y-7, 1,7);
+		g.fillRect(x, height+y-3, 3,3);
+		g.fillRect(x, height+y-2, 5,2);
+		g.fillRect(x, height+y-5, 2,5);
+		
+		/*Draws the mouth*/
+		a++;
+		if(a > 180) {
+			a = 0;
+		}
+		if((a > 30 && a < 59) || (a > 150 && a < 180)) {
+			g.fillRect(x, y+8, 2,4);
+			g.fillRect(x, y+9, 7,2);
+		}
+		else if((a > 60 && a < 89) || (a > 120 && a < 149)) {
+			g.fillRect(x, y+8, 5,4);
+			g.fillRect(x, y+9, 9,2);
+		}
+		else if(a>90&&a<119) {
+			g.setColor(Color.BLACK);
+			g.fillRect(x, y+7, 5,6);
+			g.fillRect(x, y+8, 10,4);
+			g.fillRect(x, y+9, 12,2);	
+		}
 	}
 }
