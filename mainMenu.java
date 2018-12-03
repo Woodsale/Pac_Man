179aa9207fbc605ae21bb99010d719d4d47b7ec7
burksh <36054116+burksh@users.noreply.github.com>
2018-12-03 17:02:53 -0500

    Revert "Delete mainMenu.java"
    This reverts commit 994035e3a1e98b9358889a8ef2f025f19842d709.

---
+++ mainMenu.java
@@ -0,0 +1,136 @@
+
+/**
+ * Write a description of class Image here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class mainMenu extends JFrame implements ActionListener
+{
+    private JButton startGame, Exit;
+
+    private JMenuBar menu;
+    private JMenu options;
+    private JMenuItem quit, help; 
+
+    private JLabel difficulty;
+    private JCheckBox easy, medium, hard;
+    
+    private ButtonGroup difficultyChoice;
+    
+    private Game game;
+
+    public mainMenu()
+    {   
+    	Dimension d = new Dimension(Game.WIDTH,Game.HEIGHT);
+		setPreferredSize(d);
+		setMinimumSize(d);
+		setMaximumSize(d);
+
+        setLayout(new BorderLayout());
+        setContentPane(new JLabel(new ImageIcon("pac-man-minimalist-featured-image-1024x576.png")));
+        setLayout(new FlowLayout());
+
+        /**set the layout to GridBag*/
+        setLayout(new GridBagLayout());
+        GridBagConstraints loc = new GridBagConstraints(); 
+
+        loc = new GridBagConstraints();
+        loc.gridx = 1;
+        loc.gridy = 0;
+        loc.insets.top = 20;
+        loc.insets.bottom = 20;
+        difficulty = new JLabel("Select Difficulty");
+        add(difficulty, loc);
+
+        loc = new GridBagConstraints();
+        loc.gridx = 0;
+        loc.gridy = 1;
+        loc.insets.bottom = 20;
+        easy = new JCheckBox("Easy");
+        add(easy, loc);
+
+        loc = new GridBagConstraints();
+        loc.gridx = 1;
+        loc.gridy = 1;
+        loc.insets.bottom = 20;
+        medium = new JCheckBox("Medium");
+        add(medium, loc);
+
+        loc = new GridBagConstraints();
+        loc.gridx = 2;
+        loc.gridy = 1;
+        loc.insets.bottom = 20;
+        hard = new JCheckBox("Hard");
+        add(hard, loc);
+        
+        loc = new GridBagConstraints();
+        loc.gridx = 1;
+        loc.gridy = 2;
+        loc.insets.top = 20;
+        startGame = new JButton("Start Game!");
+        add(startGame, loc);
+        
+        /**Allows for only one difficulty to 
+         * be chosen*/
+        difficultyChoice = new ButtonGroup();
+        difficultyChoice.add(easy);
+        difficultyChoice.add(medium);
+        difficultyChoice.add(hard);
+
+        
+        easy.addActionListener(this);
+        medium.addActionListener(this);
+        hard.addActionListener(this);
+        startGame.addActionListener(this);
+
+        setUpMenus();
+    }
+
+    private void setUpMenus(){
+        options = new JMenu("Options");
+        quit = new JMenuItem("Quit");
+        help = new JMenuItem("Help");
+        options.add(quit);
+        options.add(help);
+        menu = new JMenuBar();
+        setJMenuBar(menu);
+        menu.add(options);
+
+        options.addActionListener(this);
+        quit.addActionListener(this);
+        help.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        // put your code here
+        JComponent buttonPressed = (JComponent) e.getSource();
+        if (buttonPressed == startGame) {
+		if(easy.isSelected()||medium.isSelected()||hard.isSelected()){
+        	this.dispose();
+        	Game game = new Game();
+			JFrame frame = new JFrame();
+			frame.setTitle(Game.TITLE);
+			frame.add(game);
+			frame.setResizable(false);
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+			frame.requestFocus();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			game.start();
+			}
+		else JOptionPane.showMessageDialog(null, "Please select a difficulty");
+        }
+        
+        else if(buttonPressed == quit){
+            System.exit(0);
+        }
+ 
+    }
+}
